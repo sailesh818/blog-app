376ac7e77f0fd34b17429233e0a9b772b57487c1
sailesh818 <sailesh7019@gmail.com>
2026-04-09 12:04:08 +0545

    final delete added and completed

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("draft-list/", views.draft_list, name="draft-list"),
     path("draft-detail/<int:pk>/", views.draft_detail, name="draft-detail"),
     path("post-create/", views.post_create, name="post-create"),
+    path("post-update/<int:pk>/", views.post_update, name="post-update"),
+    path("draft-publish/<int:pk>/", views.draft_publish, name="draft-publish"),
+    path("post-delete/<int:pk>/", views.post_delete, name="post-delete"),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,7 @@
+
+
+from django.utils import timezone
+
 from django.shortcuts import redirect, render
 
 from blog_app.forms import PostForm
@@ -8,7 +12,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_at__isnull=False)
+    posts = Post.objects.filter(published_at__isnull=False).order_by("-published_at")
     return render(
         request,
         "post_list.html",
@@ -65,4 +69,45 @@ def post_create(request):
                 "post_create.html",
                 {"form": form}
             )
+@login_required
+def post_update(request, pk):
+    if request.method == "GET":
+        post = Post.objects.get(pk=pk)
+        form = PostForm(instance=post)
+        return render(
+            request,
+            "post_create.html",
+            {"form": form},
+        )
+    else:
+        post = Post.objects.get(pk=pk)
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            if post.published_at:
+                return redirect("post-detail", post.pk)
+            else:
+                 return redirect("draft-detail", post.pk)
+        else:
+            return render(
+                request,
+                "post_create.html",
+                {"form": form}
+            )
+            
+@login_required
+def draft_publish(request, pk):
+    post = Post.objects.get(pk=pk, published_at__isnull=True)
+    post.published_at = timezone.now()
+    post.save()
+    return redirect("post-list")
+
+@login_required
+def post_delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    if post.published_at:
+        return redirect("post-list")
+    else:
+        return redirect("draft-list")
     
